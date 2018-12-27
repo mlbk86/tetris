90c7d54e62152fd30ca91f0cbea3572477f27b8c
Michal Blazek <mblazek@vendavo.com>
2018-12-27 11:08:14 +0100

    Side moving

--- Piece.py
+++ Piece.py
@@ -1,8 +1,9 @@
 from random import choice
 
+
 class Piece:
     SHAPES = (("cyan", (0, 0), (1, 0), (2, 0), (3, 0)),  # I
-              ("blue", (0, 0), (1, 0), (2, 0), (2, 1))   # J
+              ("blue", (0, 0), (1, 0), (2, 0), (2, 1))  # J
               )
     BLOCK_SIZE = 20
 
@@ -22,21 +23,49 @@ class Piece:
             )
             self.blocks.append(block)
 
-    def move(self, x, y):
-        for block in self.blocks:
-            self.canvas.move(block, x * Piece.BLOCK_SIZE, y * Piece.BLOCK_SIZE)
+    def move(self, direction):
+        if self.can_move_piece(direction):
+            x = Piece.get_x(direction)
+            y = Piece.get_y(direction)
 
-    def can_move_piece(self):
+            for block in self.blocks:
+                self.canvas.move(block, x * Piece.BLOCK_SIZE, y * Piece.BLOCK_SIZE)
+
+    def can_move_piece(self, direction):
         for block in self.blocks:
-            if not self.can_move_block(block):
+            if not self.can_move_block(block, direction):
                 return False
 
         return True
 
-    def can_move_block(self, block):
+    def can_move_block(self, block, direction):
         current_coords = self.canvas.coords(block)
 
-        if current_coords[3] + self.BLOCK_SIZE > self.canvas.winfo_height():
-            return False;
+        can_move = False
 
-        return True
+        if direction == "Down" and current_coords[3] + self.BLOCK_SIZE <= self.canvas.winfo_height():
+            can_move = True
+
+        if direction == "Right" and current_coords[2] + self.BLOCK_SIZE <= self.canvas.winfo_width():
+            can_move = True
+
+        if direction == "Left" and current_coords[0] - self.BLOCK_SIZE >= 0:
+            can_move = True
+
+        return can_move
+
+    @staticmethod
+    def get_x(direction):
+        return {
+            "Down": 0,
+            "Right": 1,
+            "Left": -1
+        }[direction]
+
+    @staticmethod
+    def get_y(direction):
+        return {
+            "Down": 1,
+            "Right": 0,
+            "Left": 0
+        }[direction]
--- main.py
+++ main.py
@@ -1,34 +1,49 @@
 from tkinter import *
 from MainMenu import MainMenu
 from Piece import Piece
+from random import choice
 
 
 class Main:
     def __init__(self, master):
         main_menu = MainMenu(master)
-        self.canvas = Canvas(master, width=300, height=600)
-        self.canvas.grid(row=0, pady=100)
+        self.canvas = Canvas(master, width=250, height=560)
+        self.canvas.grid(column=0, row=0, sticky=(N, W, E, S))
         self.canvas.pack()
         self.current_piece = None
         self.xMove = 0
         self.yMove = 1
         self.master = master
+        self.master.bind("<Key>", self.handle_move)
 
     def add_piece(self):
         self.current_piece = Piece(canvas=self.canvas)
 
-    def move_down(self):
-        if self.current_piece.can_move_piece():
-            self.current_piece.move(self.xMove, self.yMove)
-            self.master.after(500, self.move_down)
+    def move_fall(self):
+        self.current_piece.move("Down")
+        # self.move_side()
+
+        self.master.after(500, self.move_fall)
+
+    def move(self, direction):
+        # sides = ("right", "left")
+        # side = choice(sides)
+        self.current_piece.move(direction)
+
+    # handle key presses
+    def handle_move(self, event):
+        if event.keysym == "Left":
+            self.move("Left")
+        if event.keysym == "Right":
+            self.move("Right")
+        if event.keysym == "Down":
+            self.move("Down")
 
 
 root = Tk()
+root.geometry("250x560")
 main = Main(root)
 main.add_piece()
 root.update()
-main.move_down()
+main.move_fall()
 root.mainloop()
-
-
-
